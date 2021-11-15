b334840d998d328f346017ac31167d50c90b8d33
Patrik Mäsiar <patrik@kontentino.com>
2021-11-15 21:05:01 +0100

    Fix conditions

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 module.exports = Logger = {
   log: function(...args) {
-    if (process.env.NODE_ENV === 'production') {
-      return console.log(...args);
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(...args);
     }
   },
   info: function(...args) {
-    if (process.env.NODE_ENV === 'production') {
-      return console.info(...args);
+    if (process.env.NODE_ENV !== 'production') {
+      console.info(...args);
     }
   },
   warn: function(...args) {
-    if (process.env.NODE_ENV === 'production') {
-      return console.warn(...args);
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(...args);
     }
   },
   error: function(...args) {
-    if (process.env.NODE_ENV === 'production') {
-      return console.error(...args);
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(...args);
     }
   },
 };
