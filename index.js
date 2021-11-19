db362329703a0a6838849bd22e4a55c71b0f662b
Patrik Mäsiar <patrik@kontentino.com>
2021-11-19 15:29:06 +0100

    Change conditions

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 
 module.exports = Logger = {
   log: function(...args) {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'development') {
       console.log(...args);
     }
   },
   info: function(...args) {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'development') {
       console.info(...args);
     }
   },
   warn: function(...args) {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'development') {
       console.warn(...args);
     }
   },
   error: function(...args) {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'development') {
       console.error(...args);
     }
   },
